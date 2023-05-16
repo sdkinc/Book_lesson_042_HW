22521e2d7d33eb4b6b2bc73ad17f8976fa792c5e
Olexander Zhurba <starkyzh@gmail.com>
2023-05-16 16:45:25 +0200

    solve Task

---
+++ src/Book.java
@@ -0,0 +1,72 @@
+import java.util.Objects;
+
+public class Book implements Comparable<Book> {
+  private String author;
+  private String title;
+  private int pagesCount;
+
+  public Book(String author, String title, int pagesCount) {
+    this.author = author;
+    this.title = title;
+    this.pagesCount = pagesCount;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getPagesCount() {
+    return pagesCount;
+  }
+
+  public void setPagesCount(int pagesCount) {
+    this.pagesCount = pagesCount;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Book book = (Book) o;
+
+    if (!Objects.equals(author, book.author)) return false;
+    return Objects.equals(title, book.title);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = author != null ? author.hashCode() : 0;
+    result = 31 * result + (title != null ? title.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public int compareTo(Book o) {
+    if (!author.equals(o.author)) {
+      return author.compareTo(o.author);
+    }
+    return title.compareTo(o.title);
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+            "author='" + author + '\'' +
+            ", title='" + title + '\'' +
+            ", pagesCount=" + pagesCount +
+            '}';
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    Book book3 = new Book("Bulgakov", "Master i Margarita", 300);
+    Book book1 = new Book("Jules Gabriel Verne", "Vingt mille lieues sous les mers", 420);
+    Book book2 = new Book("Bulgakov", "Beg", 250);
+
+
+    List<Book> bookList = new ArrayList<>();
+    bookList.add(book1);
+    bookList.add(book2);
+    bookList.add(book3);
+    for (Book book : bookList
+    ) {
+      System.out.println(book);
+    }
+    Collections.sort(bookList);
+    System.out.println("++++++++++++++++++++++");
+    for (Book book : bookList
+    ) {
+      System.out.println(book);
+    }
+  }
+}
